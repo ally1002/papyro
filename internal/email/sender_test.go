39e0c4942d46c7e7e9a10aecb6a06530c194ac00
aly <alyssonrosa1@gmail.com>
2026-04-03 14:25:33 -0300

    email send test

--- internal/email/sender_test.go
+++ internal/email/sender_test.go
@@ -4,28 +4,14 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
+
+	smtpmock "github.com/mocktools/go-smtp-mock/v2"
+	"github.com/wneessen/go-mail"
 
 	"github.com/ally1002/papyro/internal/profile"
 	"github.com/stretchr/testify/require"
 )
-
-func setup(t *testing.T) *Email {
-	t.Helper()
-
-	tempDir, _ := os.MkdirTemp("", "papyro-test")
-	file, _ := os.CreateTemp(tempDir, "fileToSend_*.txt")
-
-	profile := profile.Profile{Name: "aly", FromEmail: "aly@aly.com", KindleEmail: "aly@kindle.com"}
-	password := []byte("12344321")
-	filePath := file.Name()
-
-	email, err := NewEmail(profile, password, filePath)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return email
-}
 
 func TestNewEmail_ValidateFileSize(t *testing.T) {
 	profile := profile.Profile{Name: "aly", FromEmail: "aly@aly.com", KindleEmail: "aly@kindle.com"}
@@ -175,4 +161,64 @@ func TestNewEmail_ValidateFileExtension(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestEmail_Send(t *testing.T) {
+	profile := profile.Profile{Name: "aly", FromEmail: "aly@aly.com", KindleEmail: "aly@kindle.com"}
+	password := []byte("12344321")
+
+	tempDir, err := os.MkdirTemp("", "papyro-test")
+	require.NoError(t, err, "failed to create temp dir")
+
+	file, err := os.CreateTemp(tempDir, "fileToSend_*.txt")
+	require.NoError(t, err, "failed to create temp file")
+
+	_, err = file.Write([]byte("Hello world"))
+	require.NoError(t, err)
+	err = file.Close()
+	require.NoError(t, err)
+
+	t.Cleanup(func() { _ = os.RemoveAll(tempDir) })
+
+	server := smtpmock.New(smtpmock.ConfigurationAttr{
+		LogToStdout:              false,
+		LogServerActivity:        false,
+		MultipleMessageReceiving: true,
+	})
+
+	err = server.Start()
+	require.NoError(t, err, "smtp server did not start correctly")
+
+	testClient, err := mail.NewClient("127.0.0.1",
+		mail.WithPort(server.PortNumber()),
+		mail.WithTLSPortPolicy(mail.NoTLS),
+		mail.WithHELO("localhost"),
+	)
+	require.NoError(t, err, "mail client not created")
+
+	email := &Email{profile: profile, password: password, filePath: file.Name(), client: testClient}
+
+	err = email.Send()
+	require.NoError(t, err)
+
+	err = testClient.Close()
+	require.NoError(t, err)
+
+	_, err = server.WaitForMessages(1, 1*time.Second)
+	require.NoError(t, err, "timeout waiting for messages")
+
+	messages := server.Messages()
+	require.NotEmpty(t, messages, "No messages received by mock server")
+
+	msg := messages[0]
+	require.True(t, msg.Msg(), "message should be received")
+	require.True(t, msg.Data(), "DATA command should be successful")
+	require.True(t, msg.Mailfrom(), "MAIL FROM should be successful")
+	require.True(t, msg.Rcptto(), "RCPT TO should be successful")
+
+	msgContent := msg.MsgRequest()
+	require.Contains(t, msgContent, "Here is your desired content.")
+	require.Contains(t, msgContent, "Email sent by Papyro")
+	require.Contains(t, msgContent, "aly@aly.com")
+	require.Contains(t, msgContent, "aly@kindle.com")
 }
