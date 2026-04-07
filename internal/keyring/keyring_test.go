88df7c40ed5ba83441869f202eef200c8ceaef8b
aly <alyssonrosa1@gmail.com>
2026-04-07 13:30:40 -0300

    other tests

--- internal/keyring/keyring_test.go
+++ internal/keyring/keyring_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeyring_Save(t *testing.T) {
+func TestSaveAndGet(t *testing.T) {
 	kr := &Keyring{ring: keyring.NewArrayKeyring(nil)}
 
 	err := kr.Save("test-account", "secret123")
@@ -19,4 +19,24 @@ func TestKeyring_Save(t *testing.T) {
 
 	assert.Equal(t, item.Key, "test-account")
 	assert.Equal(t, item.Data, []byte("secret123"))
+}
+
+func TestGetNotFound(t *testing.T) {
+	kr := &Keyring{ring: keyring.NewArrayKeyring(nil)}
+
+	_, err := kr.Get("non-existent")
+	require.Error(t, err)
+}
+
+func TestDelete(t *testing.T) {
+	kr := &Keyring{ring: keyring.NewArrayKeyring(nil)}
+
+	err := kr.Save("test-account", "secret123")
+	require.NoError(t, err)
+
+	err = kr.Delete("test-account")
+	require.NoError(t, err)
+
+	_, err = kr.Get("test-account")
+	require.Error(t, err)
 }
